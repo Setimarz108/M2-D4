796b2a49553ba81ad168bafb74e764282d8cd988
Setimarz108 <66173614+Setimarz108@users.noreply.github.com>
2021-11-11 17:41:20 +0100

    add guest fixed

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
 
 let addGuest = document.querySelector('.addGuest')
-  let list = document.getElementsByClassName('list-group')
-let newName = document.querySelector('.guestName').value;
+  let list = document.querySelector('.list-group')
+ 
+  addGuest.addEventListener('click', function(){
 
-
-const addNewGuest = function(){
-
-    addGuest.addEventListener('click', function(){
-
-          
-        for(let i = 0; i < list.length; i++){
-                    
-        let newNodeList = document.createElement('li')
+    let newNodeList = document.createElement('li')
         newNodeList.classList.add('list-group-item')
-        newNodeList.innerHTML = newName
-        list[i].appendChild(newNodeList)
-
-        console.log(newNodeList[i])
-         
-    }
-
+        newNodeList.innerHTML = document.querySelector('.guestName').value;
+        list.appendChild(newNodeList)
+      
+          
     })
-        
+
+
+
+const resetList = function (params) {
+    
 }
 
-
-addNewGuest()
 
 
